aa815e184c272892840ac840662a3d7756c58ad5
ggroce <ggrocedev@gmail.com>
2021-02-24 21:39:23 -0600

    function name change

--- rabbit-server.js
+++ rabbit-server.js
@@ -2,19 +2,15 @@ const express = require('express');
 // const router = express.Router();
 const cors = require('cors');
 const inventory = require('./controllers/inventory');
-const sendmail = require('./controllers/inquirymail');
+const inquirymail = require('./controllers/inquirymail');
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.get('/', (req, res) => {
-  res.send('.');
-});
-
+app.get('/', (req, res) => { res.send('.') });
 app.get('/inventory', (req, res) => { inventory.handleInventoryGet(req, res) });
-
-app.post('/sendmail', (req, res) => { sendmail.handeInquiryMail(req, res) });
+app.post('/inquirymail', (req, res) => { inquirymail.handeInquiryMail(req, res) });
 
 app.listen(process.env.PORT, () => {
   console.log(`netherlandbunnies.com backend running on port ${process.env.PORT}.`);
