a441ac463487bf0c927daae233e27f206e7a65f3
ggroce <ggrocedev@gmail.com>
2021-07-08 22:41:26 -0500

    switched from sending emails with gmail to sendgrid, as gmail tokens kept going stale too fast.

--- controllers/rabbit_inquiry.js
+++ controllers/rabbit_inquiry.js
@@ -1,23 +1,8 @@
-const nodemailer = require('nodemailer');
-const MAIL_USERNAME = process.env.GOOGLE_GMAIL_EMAIL_USER;
-const MAIL_PASSWORD = process.env.GOOGLE_GMAIL_EMAIL_PASSWORD;
-const OAUTH_CLIENTID = process.env.GOOGLE_GMAIL_OAUTH_CLIENTID;
-const OAUTH_CLIENT_SECRET = process.env.GOOGLE_GMAIL_OAUTH_CLIENT_SECRET;
-const OAUTH_REFRESH_TOKEN = process.env.GOOGLE_GMAIL_OAUTH_REFRESH_TOKEN;
+const sgMail = require('@sendgrid/mail')
 const FROM_EMAIL_ADDRESS = process.env.FROM_EMAIL_ADDRESS; 
 const TO_EMAIL_ADDRESS = process.env.TO_EMAIL_ADDRESS;
+const SENDGRID_API_KEY = process.env.SENDGRID_API_KEY;
 
-const transporter = nodemailer.createTransport({
-  service: 'gmail', 
-  auth: {
-    type: 'OAuth2', 
-    user: MAIL_USERNAME, 
-    pass: MAIL_PASSWORD, 
-    clientId: OAUTH_CLIENTID,
-    clientSecret: OAUTH_CLIENT_SECRET, 
-    refreshToken: OAUTH_REFRESH_TOKEN, 
-  }
-});
 
 const handleRabbitInquiry = (req, res) => {
   if (!req.body.custName || !req.body.custEmail || !req.body.custMessage) {
@@ -31,53 +16,28 @@ const handleRabbitInquiry = (req, res) => {
 
   //parse data here, then send 
 
-  const mailOptions = {
-    from: FROM_EMAIL_ADDRESS, 
-    to: TO_EMAIL_ADDRESS, 
-    subject: `NetherlandBunnies.com Inquiry from ${custName}`, 
-    text: `${custName}, (${custEmail}), is interested in ${rabbitName}! \n\n` 
-      + `Here is what they had to say: \n    ${custMessage}`, 
+  sgMail.setApiKey(SENDGRID_API_KEY);
+  const msg = {
+    from: FROM_EMAIL_ADDRESS,
+    to: TO_EMAIL_ADDRESS,
+    subject: `NetherlandBunnies.com Inquiry from ${custName}`,
+    text: `${custName}, (${custEmail}), is interested in ${rabbitName}! \n\n`
+      + `Here is what they had to say: \n    ${custMessage}`,
   }
-
-  transporter.sendMail(mailOptions, (err, data) => {
-    if (err) {
-      console.log("Netherlandbunnies.com: error sending mail to destination: ", err);
-      res.json({
-        status: 'error'
-      });
-    } else {
+  sgMail
+    .send(msg)
+    .then(() => {
       console.log("Netherlandbunnies.com: Inquiry email sent from user. ");
       res.json({
         status: 'success'
       });
-    }
-  });
-
-  //maybe send auto responder on success too: 
-  // transporter.sendMail(mail, (err, data) => {
-  //   if (err) {
-  //     res.json({
-  //       status: 'fail'
-  //     })
-  //   } else {
-  //     res.json({
-  //        status: 'success'
-  //     })
-  
-  //     transporter.sendMail({
-  //       from: "<your email address>",
-  //       to: email,
-  //       subject: "Submission was successful",
-  //       text: `Thank you for contacting us!\n\nForm details\nName: ${name}\n Email: ${email}\n Message: ${message}`
-  //     }, function(error, info){
-  //       if(error) {
-  //         console.log(error);
-  //       } else{
-  //         console.log('Message sent: ' + info.response);
-  //       }
-  //     });
-  //   }
-  // });
+    })
+    .catch((error) => {
+      console.error(error);
+      res.json({
+        status: 'error'
+      });
+    });
 }
 
 module.exports = {
